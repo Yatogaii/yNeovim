31f892085d06d82bde9ad1c0bf6ed99f0b12e1b9
Yatogaii <781023956@qq.com>
2022-04-27 16:29:58 +0800

    add  editorconfig && config indent line

--- lua/user/conf/indent-blankline.lua
+++ lua/user/conf/indent-blankline.lua
@@ -1,12 +1,15 @@
 vim.opt.list = true
-vim.opt.listchars:append("space:⋅")
-vim.opt.listchars:append("eol:↴")
+
+vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
 
 require("indent_blankline").setup {
-    space_char_blankline = " ",
     char_highlight_list = {
-        "IndentBlanklineIndent1",
         "IndentBlanklineIndent2",
         "IndentBlanklineIndent3",
+        "IndentBlanklineIndent4",
     },
+    -- indent_size = 2,
+
 }
