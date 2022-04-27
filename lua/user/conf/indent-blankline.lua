f263407ade70a2a365267913fc88cdd20a13646d
Yatogaii <781023956@qq.com>
2022-04-27 16:33:33 +0800

    delete indent hightlight

--- lua/user/conf/indent-blankline.lua
+++ lua/user/conf/indent-blankline.lua
@@ -1,15 +1,6 @@
 vim.opt.list = true
 
-vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
-
 require("indent_blankline").setup {
-    char_highlight_list = {
-        "IndentBlanklineIndent2",
-        "IndentBlanklineIndent3",
-        "IndentBlanklineIndent4",
-    },
     -- indent_size = 2,
 
 }
